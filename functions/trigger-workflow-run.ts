fb735f7bb86f5652c728faaa9e39aea00c677b96
21vidya <21vidya29@gmail.com>
2026-08-10 14:21:28 +0530

    Fix function CORS

--- functions/trigger-workflow-run.ts
+++ functions/trigger-workflow-run.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
 
 export default async function handler(req: Request, res: Response) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false,
